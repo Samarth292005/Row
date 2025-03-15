05d2d8f9aab6351de5aaa267f1c2570e73e718a9
Samarth292005 <samarthagarwal6277@gmail.com>
2025-03-15 16:57:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of a simple script to smooth-scroll to sections when links are clicked
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
